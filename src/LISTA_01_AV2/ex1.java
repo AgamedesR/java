08b085cba05c134145955e10921e1e57fa996c5c
Agamedes <agamedesrmeira@gmail.com>
2026-05-13 07:22:29 -0300

    Lista de exercicio 01 AV2

--- src/LISTA_01_AV2/ex1.java
+++ src/LISTA_01_AV2/ex1.java
@@ -1,4 +1,13 @@
 package LISTA_01_AV2;
 
 public class ex1 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 300; i++) {
+            System.out.print(i);
+            if (i < 300) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
 }
